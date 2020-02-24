3303d638e2329180585b58a566d10585f80a8e37
Rody10 <rodneysimbatholanah@gmail.com>
2020-02-24 10:51:09 +0200

    created nunit test project

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Text;
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter string: ");
+            string str = Console.ReadLine();
+            string[] strArray = str.Split();
+          
+            int CountWords(string[] str)
+            {
+                int numWords = strArray.Length;
+                return numWords;
+            }
+            int CountLetters(string[] str)
+            {
+                int numLetters = 0;
+
+                for (int i =0 ; i < strArray.Length ; i++)
+                {
+                    numLetters = numLetters+strArray[i].Length;
+                }
+                return numLetters;
+
+            }
+            Console.WriteLine(CountLetters(strArray)+","+CountWords(strArray)+":"+str);
+            Console.Read();
         }
+    
     }
 }
